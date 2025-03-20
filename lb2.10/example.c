07753788f0b5e5b6dddf7f5e87d35c49553d8b8e
MrAnt22 <makslipovyi@gmail.com>
2025-03-20 23:17:59 +0200

    lb2.10

---
+++ lb2.10/example.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void hello(){
+	printf("Shared library\n");
+
+}
+
+int add(int a, int b){
+	return a+b;
+
+}
---
+++ lb2.10/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "example.h"
+
+int main(){
+
+	printf("10+20 = %d\n", add(10,20));
+	hello();
+	return 0;
+}
